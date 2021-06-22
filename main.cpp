67a1c4a81fcae9b3d0b205e1a4236f323e06e314
KillToTheReal <73242190+KillToTheReal@users.noreply.github.com>
2021-06-22 20:40:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,23 +4,9 @@ using namespace std;
 
 int main()
 {
-    Table<float> bruh(4,5);
+    Table<float> test(4,5);
 
-    bruh.InsertInto(0,1,15.21);
-
-    bruh.Info();
-    bruh.AddRows(3, 6.23);
-    bruh.AddRows(2,7.6);
-    bruh.AddColumns(2,8.5);
-    bruh.RemoveColumns(1);
-    bruh.Info();
-    bruh.AddColumns(2,17,3);
-    bruh.WriteToFile("example","csv");
-    bruh.Info();
-    bruh.Find(0);
-    cout<<endl<<bruh.ArithmeticalMeanRow(2);
-    cout<<endl<<bruh.ArithmeticalMeanCol(4);
-    cout<<endl<<bruh.SumCol(4);
-    cout<<endl<<bruh.SumRow(4);
+    unit_test();
+    
     return 0;
 }
